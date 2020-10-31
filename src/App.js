21e71f7316c2235ed810d73f4ff216a8278d72ad
DongKee <eastflag@gmail.com>
2020-10-31 10:00:40 +0900

    Counter, Player 컴포넌트 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,55 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
-
-const Player = (props) => (
-  <div className='player'>
-    <span className='player-name'>
-        <button className='remove-player' onClick={() => props.removePlayer(props.id)}>x</button>
-    </span>
-    <span className='player-name'>{props.name}</span>
-    <Counter/>
-  </div>
-);
-
-class Counter extends React.Component {
-  state = {
-    score: 0,
-    a: 3
-  }
-
-  constructor(props) {
-    super(props);
-    // 1) this.incrementScore = this.incrementScore.bind(this);
-  }
-
-  changeScore = (delta) => {
-    // 2) arrow 펑션안의 this는 lexical this
-    console.log(this);
-    // 1. state를 변경하는 방법
-    // this.state.score += 1;
-    // this.setState({score: this.state.score + 1});
-    // 2. merge 된다. : 기존 속성으 그대로 유지
-    // 3. 비동기로 처리
-    this.setState(prevState => ({
-      score: prevState.score + delta
-    }));
-  }
-
-  render() {
-    return (
-      <div className='counter'>
-        <button className='counter-action decrement'
-                onClick={() => this.changeScore(-1)}> -
-        </button>
-        <span className='counter-score'>{this.state.score}</span>
-        <button className='counter-action increment'
-                onClick={() => this.changeScore(1)}> +
-        </button>
-      </div>
-    );
-  }
-}
+import Player from "./components/Player";
 
 class App extends React.Component {
   state = {
